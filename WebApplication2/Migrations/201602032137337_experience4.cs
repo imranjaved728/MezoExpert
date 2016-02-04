1946c9f8ac6eb9eff71d89619cef74fe0a02a886
Imran <imranjaved728@gmail.com>
2016-02-03 19:49:00 -0800

    commiting

---
+++ WebApplication2/Migrations/201602032137337_experience4.cs
@@ -0,0 +1,18 @@
+namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class experience4 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Tutors", "IsCompletedProfile", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tutors", "IsCompletedProfile", c => c.Int(nullable: false));
+        }
+    }
+}
